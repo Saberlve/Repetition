4ab29dbcd0563ea83070c589e7a5e21b6e2da461
Saberlve <saberlve9@gmail.com>
2025-04-16 21:08:50 +0800

    construct contrastive pairs

---
+++ shell/create_contrastive_pairs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+icl_dataset_path='../dataset/icl_dataset.json'
+raw_dataset_path='../raw_dataset/minipile_train.json'
+output_path='../dataset/contrastive_dataset.json'
+
+python ../DPO/generate_datasets.py
+--icl_dataset_path $icl_dataset_path \ 
+--raw_dataset_path $raw_dataset_path \ 
+--output_path $output_path
