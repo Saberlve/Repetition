5e67d49f0f094127b36a54db3ea49306848ecb08
Saberlve <saberlve9@gmail.com>
2025-04-17 16:43:03 +0800

    init steer

--- process_dataset.py
+++ process_dataset.py
@@ -3,8 +3,8 @@ import json
 
 
 
-data_path = 'minipile_train.json'
-save_path = './repetition_experiment/prompt'
+data_path = 'raw_dataset/minipile_test.json'
+save_path = 'prompt'
 os.makedirs(save_path, exist_ok=True)
 
 with open(data_path, 'r',encoding='utf-8') as f:
@@ -17,5 +17,6 @@ for i, item in enumerate(data):
     
 save_path = os.path.join(save_path, data_path.split('/')[-1])
 with open(save_path, 'w',encoding='utf-8') as f:
+    print(f'truncate {len(save_data)} items to {save_path}')
     json.dump(save_data, f, indent=4, ensure_ascii=False)
     
