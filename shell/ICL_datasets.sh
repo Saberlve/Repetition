da1a73a958a09eb9d1916c855d96a13555b5caa6
Saberlve <saberlve9@gmail.com>
2025-04-21 21:00:44 +0800

    update dataset

--- shell/ICL_datasets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-natural_prompts_path='../dataset/natural_prompts.json'
-output_path='../dataset/icl_dataset.json'
-
-python ../ICL/prepare_ICL_datasets.py \
---natural_prompts_path $natural_prompts_path \
---output_path $output_path 
--- shell/gemma/dpo.sh
+++ shell/gemma/dpo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 cd ../../LLaMA-Factory
-# llamafactory-cli train examples/train_lora/qwen2.5_lora_dpo.yaml
-llamafactory-cli train examples/train_lora/gemma2_lora_dpo.yaml
+CUDA_VISIBLE_DEVICES=1 llamafactory-cli train examples/train_lora/gemma2_lora_dpo.yaml
